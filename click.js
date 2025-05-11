101f8e55f620bea60c744198c943a9fa4b5d32d9
Fadhiel Naufan <fadhielnaufan@gmail.com>
2025-05-11 10:03:25 +0700

    Initial commit

--- click.js
+++
@@ -1,8 +0,0 @@
-
-var pesen="HAYO MAU NGAPAIN LU TONG? HAHAHA";
-///////////////////////////////////
-function clickIE4(){if (event.button==2){alert(pesen);return false;}}
-function clickNS4(e){if (document.layers||document.getElementById&&!document.all){if (e.which==2||e.which==3){alert(pesen);return false;}}}
-if (document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;}
-else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;}
-document.oncontextmenu=new Function("alert(pesen);return false")
